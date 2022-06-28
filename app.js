ecf5f8c317d8e8f35e5de0c1ea925a50f732738b
samuel cordova <cordovas1337@gmail.com>
2022-06-28 15:39:10 -0500

    added the for loop to and code neded to grab all four points of the block for collision detection

--- app.js
+++ app.js
@@ -19,20 +19,47 @@ class Block {
 
 }
 
+    // create an array of all my blocks. 
+    const blocks = [
+        // setting the x and y coordinates
+        new Block(10, 270)
+    ]
+
+    // console log to make sure it's running properly should grab all four points that create the block. 
+    console.log(blocks[0])
+
+
 
 
 // function to draw the block, will have to repeat to draw other blocks.
-function addBlock() {
+// function addBlock() {
 
     // create the first block div and add a class of block to it
-    const block= document.createElement('div');
-    block.classList.add('block');
+    // const block= document.createElement('div');
+    // block.classList.add('block');
     // adding the left and the bottom styles and spacing to the css in javascript
-    block.style.left = '100px'
-    block.style.bottom = '50px'
+    // block.style.left = '100px'
+    // block.style.bottom = '50px'
     // Add the element to a child of grid.
-    grid.appendChild(block);
+    // grid.appendChild(block);
 
+// }
+// same as the single block function more or less but plural for multiple blocks.
+function addBlocks() {
+    // for loop to create all the blocks I need to draw
+    for (let i = 0; i < blocks.length; i++) {
+
+         // create the first block div and add a class of block to it
+        const block= document.createElement('div');
+        block.classList.add('block');
+        // adding the left and the bottom styles and spacing to the css in javascript
+        // grab the bottomLeft xAxis from the constructor above using [0]  to refer to xAxis that we set up in this.bottomLeft.
+        block.style.left = blocks[i].bottomLeft[0]
+        block.style.bottom = blocks[i]
+        // Add the element to a child of grid.
+        grid.appendChild(block);
+    }
+ 
 }
 
-addBlock()
+addBlocks()
