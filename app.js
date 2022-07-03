e835dc7da3ed97af061a4e8b10083d83a8aa2949
samuel cordova <cordovas1337@gmail.com>
2022-07-03 17:00:44 -0500

    added some more styles to the you win and game over screen, will work on it more in the future

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
 const ballDiameter = 20
-const boardWidth = 560 
+const boardWidth = 560
 const boardHeight = 300
 //set a timerID for ball
 let timerId
-// set variables for xDirection and yDirection thast we can use later to relate to how fast the balls will move in either direction
+// set variables for xDirection and yDirection that we can use later to relate to how fast the balls will move in either direction
 let xDirection = 2
 let yDirection = 2
-// let xDirection = -2
-// let yDirection = -2
 let score = 0
 
 
@@ -22,8 +20,8 @@ const userStart = [230, 10]
 let currentPosition = userStart
 
 // set up positioning for the ball, want it to sit right on top of the user start position.
-const ballStart =[270, 40]
-let ballCurrentPosition = ballStart 
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
 
 
 
@@ -43,55 +41,38 @@ class Block {
 
 }
 
-    // create an array of all my blocks. 
-    const blocks = [
-        // setting the x and y coordinates in relation to the constructor above.
-        new Block(10, 270),
-        new Block(120, 270),
-        new Block(230, 270),
-        new Block(340, 270),
-        new Block(450, 270),
-        
-        new Block(10, 240),
-        new Block(120, 240),
-        new Block(230, 240),
-        new Block(340, 240),
-        new Block(450, 240),
-        
-        new Block(10, 210),
-        new Block(120, 210),
-        new Block(230, 210),
-        new Block(340, 210),
-        new Block(450, 210),
-        
-    ]
+// create an array of all my blocks. 
+const blocks = [
+    // setting the x and y coordinates in relation to the constructor above.
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
 
-    // console log to make sure it's running properly should grab all four points that create the block. 
-    // console.log(blocks[0])
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+
+]
 
 
 
-
-// function to draw the block, will have to repeat to draw other blocks.
-// function addBlock() {
-
-    // create the first block div and add a class of block to it
-    // const block= document.createElement('div');
-    // block.classList.add('block');
-    // adding the left and the bottom styles and spacing to the css in javascript
-    // block.style.left = '100px'
-    // block.style.bottom = '50px'
-    // Add the element to a child of grid.
-    // grid.appendChild(block);
-
-// }
 // same as the single block function more or less but plural for multiple blocks.
 function addBlocks() {
     // for loop to create all the blocks I need to draw based on the array above.
     for (let i = 0; i < blocks.length; i++) {
 
-         // create the first block div and add a class of block to it
-        const block= document.createElement('div');
+        // create the first block div and add a class of block to it
+        const block = document.createElement('div');
         block.classList.add('block');
         // adding the left and the bottom styles and spacing to the css in javascript
         // grab the bottomLeft xAxis from the constructor above using [0]  to refer to xAxis that we set up in this.bottomLeft.
@@ -99,17 +80,17 @@ function addBlocks() {
         block.style.bottom = blocks[i].bottomLeft[1] + 'px'
         // Add the element to a child of grid.
         grid.appendChild(block);
-        
-      
-    }
-     // A way to automate the x coordinates need to figure out how to run it in increments of 5 and then start over..
-//  if( i < blocks.length ) {
-//     block.style.left = blocks[i].bottomLeft[0] + 110 * i + 'px'
-//     block.style.bottom = blocks[i].bottomLeft[1] + 'px'
 
-// }
-// console.log(blocks[i].bottomLeft[0] + 110 * i);
- 
+
+    }
+    // A way to automate the x coordinates need to figure out how to run it in increments of 5 and then start over..
+    //  if( i < blocks.length ) {
+    //     block.style.left = blocks[i].bottomLeft[0] + 110 * i + 'px'
+    //     block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+
+    // }
+    // console.log(blocks[i].bottomLeft[0] + 110 * i);
+
 }
 
 addBlocks()
@@ -133,35 +114,35 @@ function drawUser() {
 
 // Draw the ball
 function drawBall() {
-       // both of these will print based on Currents position array.
-       ball.style.left = ballCurrentPosition[0] + 'px'
-       ball.style.bottom = ballCurrentPosition[1] + 'px'
-   }
+    // both of these will print based on Currents position array.
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
 
 // Move user.
 
 function moveUser(e) {
     // will use a switch case because we are going to be listening out for different keys.
     // pass through what to listen for and if it matches the case we will execute the code and breakout.
-    switch(e.key) {
+    switch (e.key) {
         // if the key pressed has the value of ArrowLeft then...
         case 'ArrowLeft':
             // add an if statement so that the xAxis will never go past the border of the grid (will never be less than 0)
             if (currentPosition[0] > 0) {
-        // subtract ten from xAxis on keypress.
-        currentPosition[0] -=10
-        // redraw the user.
-        drawUser()
+                // subtract ten from xAxis on keypress.
+                currentPosition[0] -= 10
+                // redraw the user.
+                drawUser()
             }
-        break;
+            break;
         // second switch case
         case 'ArrowRight':
             // as long as the x axis doesn't extend past boardWidth (need to subtract the block width to account for anchor point.)
             if (currentPosition[0] < boardWidth - blockWidth) {
                 // Ad 10 to the xAxis on keydown
-            currentPosition[0] +=10
-            // redraw the user.
-            drawUser()
+                currentPosition[0] += 10
+                // redraw the user.
+                drawUser()
             }
             break;
     }
@@ -179,7 +160,7 @@ grid.appendChild(ball)
 
 // Move the ball
 function moveBall() {
-   
+
     // want our ball to move by adding x and y axis
     ballCurrentPosition[0] += xDirection
     ballCurrentPosition[1] += yDirection
@@ -192,9 +173,9 @@ function moveBall() {
 timerId = setInterval(moveBall, 30)
 
 //check for collisions
-function checkForCollisions() { 
+function checkForCollisions() {
     // Check for ball collisions --------
-    for(let i =0 ; i < blocks.length; i++) {
+    for (let i = 0; i < blocks.length; i++) {
         //have to check if ball hits inbetween bottom right and left xAxis of the block.
         if (
             // if ball current position xAxis is larger than the blocks xAXis or smaller than the bottom right's xAxis
@@ -212,13 +193,11 @@ function checkForCollisions() {
             changeDirection()
             // if you hit a block add to the score and update display.
             score++
-            scoreDisplay.innerHTML= score
+            scoreDisplay.innerHTML = score
 
             // check for a win
-            if(blocks.length === 0) {
-                scoreDisplay.innerHTML= "You win!!"
-                clearInterval(timerId)
-                document.removeEventListener('keydown', moveUser)
+            if (blocks.length === 0) {
+                stopGame()
 
             }
         }
@@ -226,48 +205,40 @@ function checkForCollisions() {
 
 
     // check if ball collides with wall on the x axis. (is wider than the board width)----------
-    if(ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
-    // if the ball collides with y axis, is = to the board height.
-    ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
-    // If x axis is smaller than or equal to zero than run changeDirection
-    ballCurrentPosition[0] <= 0 ) {
+    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
+        // if the ball collides with y axis, is = to the board height.
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+        // If x axis is smaller than or equal to zero than run changeDirection
+        ballCurrentPosition[0] <= 0) {
         changeDirection()
     }
 
     // check for user collisions (make ball bounce off user if hit)
 
     // if ball current position xAxis is larger than the current position of our users xAxis && the ball current position xAxis is smaller than the current position xAxis + userWidth  
-    if(
+    if (
         (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
         (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
-        ) {
-            changeDirection()
-        }
+    ) {
+        changeDirection()
+    }
 
     // check for game over-----------------------------------
     // If ball hits bottom of the board clear interval(stop ball from moving)
     if (ballCurrentPosition[1] <= 0) {
-        clearInterval(timerId)
-        // once the ball hits the bottom set score to "you lose"
-        scoreDisplay.innerHTML = 'You Lose'
-        // remove event listener of keydown so we can't move the user
-        document.removeEventListener('keydown', moveUser)
-        grid.classList.remove('grid')
-        grid.classList.add('none')
-        scoreDisplay.classList.add('gameover')
+        stopGame()
     }
 }
 
-function changeDirection() { 
+function changeDirection() {
     // currently our ball is moving +2 x and y +2 so..
 
-    // If xDirection is and yDirection on collision = 2
     // Trying to set a bunch of rules for the collision detection on the x and y axis ... redefining values as needed.
     if (xDirection === 2 && yDirection === 2) {
         // if ball hits the grid start descending.
-    yDirection = -2
-    return
-    } 
+        yDirection = -2
+        return
+    }
     // need a way to account for all the possible combinations of the collision and direction.
     if (xDirection === 2 && yDirection === -2) {
         xDirection = -2
@@ -286,3 +257,28 @@ function changeDirection() {
     }
 }
 
+function stopGame() {
+    clearInterval(timerId)
+    // once the ball hits the bottom set score to "you lose"
+    if (blocks.length === 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = "You win!!"
+        document.removeEventListener('keydown', moveUser)
+        grid.classList.remove('grid')
+        grid.classList.add('none')
+        scoreDisplay.classList.add('gameover')
+    }
+    if (
+
+        ballCurrentPosition[1] <= 0
+    ) {
+        clearInterval(timerId)
+        // once the ball hits the bottom set score to "you lose"
+        scoreDisplay.innerHTML = 'You Lose'
+        // remove event listener of keydown so we can't move the user
+        document.removeEventListener('keydown', moveUser)
+        grid.classList.remove('grid')
+        grid.classList.add('none')
+        scoreDisplay.classList.add('gameover')
+    }
+}
